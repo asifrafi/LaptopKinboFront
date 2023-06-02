cff90271896879646f64a08f197d52ade851d562
Sabbir Hasan <sabbirshouvo@gmail.com>
2023-06-03 01:08:32 +0300

    Update api url

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ const PriceRangeForm = () => {
   const [isGaming, setIsGaming] = useState(false);
   const [isMacBook, setIsMacBook] = useState(false);
   const [laptops, setLaptops] = useState([]);
-
+  const apiBase = "https://brain.laptopkinbo.com";
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       //let apiUrl = `http://127.0.0.1:8000/laptopinrange/${lowerPrice}/${upperPrice}/`;
-      let apiUrl = `http://localhost:5000/laptopinrange/${lowerPrice}/${upperPrice}/`;
+      let apiUrl = `${apiBase}/laptopinrange/${lowerPrice}/${upperPrice}/`;
 
       if (isGaming) {
        // apiUrl = `http://127.0.0.1:8000/gaminglaptop/${lowerPrice}/${upperPrice}/`;
-        apiUrl = `http://localhost:5000/gaminglaptop/${lowerPrice}/${upperPrice}/`;
+        apiUrl = `${apiBase}/gaminglaptop/${lowerPrice}/${upperPrice}/`;
 
       }
       
       if (isMacBook) {
        // apiUrl = `http://127.0.0.1:8000/mac/${lowerPrice}/${upperPrice}/`;
-        apiUrl = `http://localhost:5000/mac/${lowerPrice}/${upperPrice}/`;
+        apiUrl = `${apiBase}/mac/${lowerPrice}/${upperPrice}/`;
       }
       if (isGaming && isMacBook) {
         setLaptops([{ id: -1, name: "MacBooks Are Not Meant For Gaming!!", price: "যা নেই তার আবার কিসের দাম" }]);
